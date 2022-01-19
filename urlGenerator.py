e36b7cb3625d556b81476b97edf7df8dbd39d80f
Jinwook <kimjinwook94@gmail.com>
2022-01-20 03:48:21 +0900

    third commit: added many repos and API keywords

--- urlGenerator.py
+++ urlGenerator.py
@@ -8,23 +8,79 @@ from openpyxl import Workbook
 
 # Github Org Name : Github Repo Name
 repoDict = {
-    'zipkin': 'openzipkin',
+    # Popular Open Source Projects Using Spring Framework
     'generator-jhipster': 'jhipster',
+    'zipkin': 'openzipkin',
+    'piggymetrics': 'sqshq',
     'thingsboard': 'thingsboard',
+    'spring-boot-admin': 'codecentric',
+    'cas': 'apereo',
+    'spring-petclinic': 'spring-projects',
+    'flowable-engine': 'flowable',
+    'conductor': 'Netflix',
+    'kafdrop': 'obsidiandynamics',
     'sagan': 'spring-io',
+    'initializr': 'spring-io',
+    'TelegramBots': 'rubenlagu',
+    'shopizer': 'shopizer-ecommerce',
+    'cwa-server': 'corona-warn-app',
+    'BroadleafCommerce': 'BroadleafCommerce',
+    'uaa': 'cloudfoundry',
+
+    # Woowacourse Projects
+    '2021-tyf': 'woowacourse-teams',
     '2021-jujeol-jujeol': 'woowacourse-teams',
+    '2021-drop-the-code': 'woowacourse-teams',
     '2021-botobo': 'woowacourse-teams',
+    '2021-zzimkkong': 'woowacourse-teams',
+    '2021-babble': 'woowacourse-teams',
+    '2021-gpu-is-mine': 'woowacourse-teams',
     '2021-pick-git': 'woowacourse-teams',
-    '2021-darass': 'darass-team',
-    '2020-6rinkers': 'woowacourse-teams'
+    '2021-cvi': 'woowacourse-teams',
+    '2021-nolto': 'woowacourse-teams',
+    '2021-darass': 'woowacourse-teams',
+    '2021-see-you-there': 'woowacourse-teams',
+    '2020-taggle': 'woowacourse-teams',
+    '2020-doran-doran': 'woowacourse-teams',
+    '2020-legeno-around-here': 'woowacourse-teams',
+    '2020-saebyeok': 'woowacourse-teams',
+    '2020-devbie': 'woowacourse-teams',
+    '2020-zeze': 'woowacourse-teams',
+    '2020-14f-guys': 'woowacourse-teams',
+    '2020-6rinkers': 'woowacourse-teams',
+    '2020-songpa-people': 'woowacourse-teams',
+    '2020-seller-lee-company': 'woowacourse-teams',
 }
 
 # Search Keywords : Keywords Category
 keywordsDict = {
+    # Annotations
     'override': 'Annotation Default',
     'suppresswarnings': 'Annotation Default',
     'safevarargs': 'Annotation Default',
     'functionalinterface': 'Annotation Default',
+
+    # Exceptions
+
+    # Collections
+    'java.util.ArrayList': 'Collections',
+    'java.util.Vector': 'Collections',
+    'java.util.LinkedList': 'Collections',
+    'java.util.Stack': 'Collections',
+    'java.util.HashSet': 'Collections',
+    'java.util.TreeSet': 'Collections',
+    'java.util.HashMap': 'Collections',
+    'java.util.HashTable': 'Collections',
+    'java.util.TreeMap': 'Collections',
+    'java.util.Properties': 'Collections',
+
+    # Collections Related
+    'java.util.Iterator': 'Collections Related',
+    'java.util.ListIterator': 'Collections Related',
+    'java.util.Enumeration': 'Collections Related',
+    'java.util.Comparator': 'Collections Related',
+
+    #
 
 }
 
@@ -49,6 +105,5 @@ minute = str(now.minute).zfill(2)
 filename = "urlGenerator_" + month + day + "_" + hour + minute + ".xlsx"
 
 # Excel File Export
-wb.save(r'files/'+filename)
+wb.save(r'files/' + filename)
 print(filename + ", Save Completed")
-
